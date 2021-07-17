99d1d07925373f3603a404885acc2b54b1991c1e
ahamedsahib <adsahib39@gmail.com>
2021-07-17 18:52:57 +0530

    [Ahamed]Withdraw and Deposit amount from account

---
+++ DataStructurePrograms/BankingCashCounter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructurePrograms
+{
+    public class BankingCashCounter<T>
+    {
+        QueueOperation<T> queue = new QueueOperation<T>();
+        //int balance = 10000;
+
+        //Methid for performing cash Counter operations
+        public void CashCounter()
+        {
+            int count=0;
+            //list of persons to insert into the queue
+            List<string> persons = new List<string> { "Person1", "Person2", "Person3" };
+            int[] balance = { 10000,500,2000000 };
+            foreach (string i in persons)
+            {
+                T x = (T)Convert.ChangeType(i, typeof(T));
+                //insert into queue
+                queue.Enqueue(x);
+            }
+            queue.Display();
+            foreach (var details in persons)
+                {
+                    Operation(details, (int)balance.GetValue(count));
+                    queue.Display();
+                    count++;
+                }
+
+        }
+        public void Operation(string person, int balance)
+        {
+            //ask user whether he needs to deposit or withdraw
+            Console.WriteLine("\n1.Deposit\n2.Withdraw\n3.Exit\n4.Enter your option");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                //case for deposit
+                case 1:
+                    Console.WriteLine("Enter Amount to Deposit");
+                    int amount = Convert.ToInt32(Console.ReadLine());
+                    balance += amount;
+                    Console.WriteLine($"Deposited {amount} to the bank");
+                    Console.WriteLine($"Total Bank Balance --> {balance}");
+                    //remove person from the queue after his operation performed
+                    queue.Dequeue();
+                    break;
+                //case for withdraw
+                case 2:
+
+                    int amountWithdraw;
+                    while (true)
+                    {
+                        Console.WriteLine("Enter Amount to Withdraw");
+                        amountWithdraw = Convert.ToInt32(Console.ReadLine());
+                        //Check whether balance is present or not
+                        if (amountWithdraw.CompareTo(balance)>0)
+                        {
+                            Console.WriteLine("Bank Balance Insufficient");
+                        }
+                        else
+                        {
+                            balance -= amountWithdraw;
+                            break;
+                        }
+                    }
+                    Console.WriteLine($" Withdrawed {amountWithdraw} from the bank");
+                    Console.WriteLine($"Total Bank Balance --> {balance}");
+                    //remove person from the queue after his operation performed
+                    queue.Dequeue();
+                    break;
+                case 3:
+                    //remove person from the queue if he wants to exit
+                    queue.Dequeue();
+                    break;
+            }
+        }
+    }
+}
---
+++ DataStructurePrograms/QueueOperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace DataStructurePrograms
+{
+    public class QueueOperation<T>
+    {
+        Node<T> front;
+        Node<T> rear;
+        public void Enqueue(T val)
+        {
+                Node<T> temp = new Node<T>(val);
+                if (this.front == null)
+                {
+
+                    this.front = temp;
+                    this.rear = temp;
+                    temp.next = null;
+                }
+                else
+                {
+                    this.rear.next = temp;
+                    this.rear = temp;
+                }
+        }
+
+            public void Dequeue()
+            {
+                if (this.front == null)
+                {
+                    Console.WriteLine("Queue is Empty");
+                    return;
+                }
+                else
+                {
+
+                    this.front = this.front.next;
+                    return;
+                }
+
+            }
+
+            public void Display()
+            {
+                Console.WriteLine("\n");
+                Node<T> temp = this.front;
+                while (temp != null)
+                {
+                    Console.Write($"|__{temp.data}__|");
+                    temp = temp.next;
+                }
+            }
+            public int Size()
+            {
+                int count = 0;
+                Node<T> temp = this.front;
+                while (temp != null)
+                {
+                    count++;
+                    temp = temp.next;
+                }
+                return count;
+            }
+        
+    }
+}
